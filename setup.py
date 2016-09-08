64bc8d46ea171d11e10cad9a82575d0ecdeab646
Shaun McCance <shaunm@redhat.com>
2016-09-08 12:13:38 -0400

    setup.py: Check for python 3

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+import sys
+
+if sys.version_info[0] != 3:
+    sys.stderr.write("pintail-itstool requires python 3\n")
+    sys.exit(1)
+
 setup(
     name='pintail-itstool',
     version='0.2',
