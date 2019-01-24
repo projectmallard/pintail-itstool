d50f67fc04ad508a2a0f1af92a91872278cec1ca
Shaun McCance <shaunm@redhat.com>
2019-01-24 11:03:01 -0500

    Version 0.3

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ if sys.version_info[0] != 3:
 
 status, output = subprocess.getstatusoutput('itstool --version')
 if status != 0:
-    sys.stderr.write("pintail-itstool requires itstool >= 2.0.2\n")
+    sys.stderr.write("pintail-itstool requires itstool >= 2.0.3\n")
     sys.exit(1)
 version = tuple(int(x) for x in output.split()[-1].split('.'))
 if version < (2, 0, 2):
-    sys.stderr.write("pintail-itstool requires itstool >= 2.0.2\n")
+    sys.stderr.write("pintail-itstool requires itstool >= 2.0.3\n")
     sys.exit(1)
 
 setup(
     name='pintail-itstool',
-    version='0.2',
+    version='0.3',
     description='Use ITSTool to translate Pintail sites.',
     packages=['pintail', 'pintail.itstool'],
     namespace_packages=['pintail'],
